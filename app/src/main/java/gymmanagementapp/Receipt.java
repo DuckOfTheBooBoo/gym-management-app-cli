235e0b6c59b75356f09e5eb3f0aaee8772c6ed6e
Arajdian Altaf <arajdianaltaf123@gmail.com>
2024-04-01 23:56:49 +0700

    feat:  add Receipt class for receipt purposes

---
+++ app/src/main/java/gymmanagementapp/Receipt.java
@@ -0,0 +1,27 @@
+package gymmanagementapp;
+
+import javax.swing.*;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Receipt {
+    private String content = "";
+
+    public Receipt(String content) {
+        this.content = content;
+    }
+
+    public void showReceipt() {
+        JOptionPane.showMessageDialog(null, content, "Receipt", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static Receipt fromTrial(String fullName, String email, Date date, int price) {
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");    
+        String dateString = df.format(date);
+        String content = String.format("Full name: %s\nE-mail: %s\nFree Trial date: %s", fullName, email, dateString);
+        return new Receipt(content);
+
+    }
+}
