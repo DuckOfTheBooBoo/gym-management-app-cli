75699e071c789993940980dc81b81fb075c339f0
DamarWahyu111 <damarwahyup160@gmail.com>
2024-04-01 12:48:05 +0700

    Test

--- app/src/main/java/gymmanagementapp/App.java
+++ app/src/main/java/gymmanagementapp/App.java
@@ -4,6 +4,8 @@ public class App {
 
     public static void main(String[] args) {
         // Instances initialization
-        String apakek = "apakek";
-    }
+        Scanner Scan = new Scanner(System.in);
+            String 
+            System.out.prntln(---WELCOME TO GYM ESPERT);
+    } 
 }
