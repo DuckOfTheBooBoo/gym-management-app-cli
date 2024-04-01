7fcac6657008e65caaedc042503ffc495c2bec3a
Arajdian Altaf <arajdianaltaf123@gmail.com>
2024-04-02 00:13:09 +0700

    feat: add price to trial pass receipt

--- app/src/main/java/gymmanagementapp/Receipt.java
+++ app/src/main/java/gymmanagementapp/Receipt.java
@@ -20,7 +20,7 @@ public class Receipt {
     public static Receipt fromTrial(String fullName, String email, Date date, int price) {
         DateFormat df = new SimpleDateFormat("dd/MM/yyyy");    
         String dateString = df.format(date);
-        String content = String.format("Full name: %s\nE-mail: %s\nFree Trial date: %s", fullName, email, dateString);
+        String content = String.format("Full name: %s\nE-mail: %s\nFree Trial date: %s\nPrice: Rp. %d", fullName, email, dateString, price);
         return new Receipt(content);
 
     }
